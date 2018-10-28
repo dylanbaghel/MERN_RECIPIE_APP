c62dc5c8b1271ecb83871308f4ee89a29a5bb29e
dylanbaghel <baghelabhi39@gmail.com>
2018-10-28 22:15:56 +0530

    fixed local api url to remote

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://mern-recipie-app.herokuapp.com';
-export const BASE_URL = 'http://localhost:4100';
+export const BASE_URL = 'https://mern-recipie-app.herokuapp.com';
+// export const BASE_URL = 'http://localhost:4100';
